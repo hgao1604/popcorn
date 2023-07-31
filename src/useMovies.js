8a70143a017c396cef0c081336e8c8b6cdec8c6e
Han Gao <highcold@Hans-Mac-mini.attlocal.net>
2023-07-30 23:33:01 -0700

    fix the fetching problem by using https

--- src/useMovies.js
+++ src/useMovies.js
@@ -13,7 +13,7 @@ export function useMovies(query, callback) {
         setIsLoading(true);
         setError("");
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+          `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`
         );
         const data = await res.json();
         console.log(data);
